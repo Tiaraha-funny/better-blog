fb4df3cb6ddeff9e6235a32229672618a19c7d2d
unknown <jeannie.petah@gmail.com>
2020-07-12 15:16:20 +0300

    made some changes to make the new post added

--- script.js
+++ script.js
@@ -2,26 +2,34 @@ console.log('it works');
 
 //Calling the neccessary elements
 
-const container = document.querySelector('.container');
+// const container = document.querySelector('.container');
+
 const post = document.querySelector('#post-list');
 const card = document.querySelector('.card');
-const image = document.querySelector('.card-img-top');
+const cardFooter = document.querySelector('.card-footer');
+
+// const image = document.querySelector('.card-img-top');
 const cardBody = document.querySelector('.card-body');
-const header = document.querySelector('.card-title');
-const paragraph = document.querySelector('.card-text');
+
+const header = document.querySelector('#postTitle');
+const paragraph = document.querySelector('[name="postContent"]');
+const author = document.querySelector('[name="postAuthor"]');
+const image = document.querySelector('[value="https://picsum.photos/500/200"]');
+
+
 const button = document.querySelector('.btn-delete');
+
 const submitBtn = document.querySelector('.btn-primary');
 
 const input = document.querySelectorAll('.form-control');
 
+
 //creating elements
-function createNewPost () {
+const createNewPost = () => {
+
     let img = document.createComment('img');
     img.src = `https://picsum.photos/500/200`;
     img.alt = 'Card image cap';
-
-    let newPost = document.createElement('div');
-    newPost.classList.add('#post-list');
 
     let newCard = document.createElement('div');
     newCard.classList.add('card');
@@ -35,19 +43,31 @@ function createNewPost () {
     let newParagraph = document.createElement('p');
     newParagraph.classList.add('card-text');
 
-    newHeader.textContent = `${header}`;
-    newParagraph.textContent = `${paragraph}`;
+    let deletebtn = document.createElement('button');
+    deletebtn.classList.add('btn-delete');
+
+    let newDateFooter = document.createElement('div');
+    newDateFooter.classList.add('.card-footer');
+
+    newHeader.textContent = `${header.value} by ${author.value}`;
+    newParagraph.textContent = `${paragraph.value}`;
+    img.textContent = `${image.value}`;
+    newDateFooter.textContent = `${cardFooter.value}`;
 
     // append child of those elements
-    newPost.appendChild(newCard);
+    post.appendChild(newCard);
     newCard.appendChild(img);
     newCard.appendChild(newCardBody);
     newCardBody.appendChild(newHeader);
     newCardBody.appendChild(newParagraph);
+    newCardBody.appendChild(deletebtn);
+    newCard.appendChild(newDateFooter);
 
+    return newCard;
 }
 
+
 submitBtn.addEventListener('click', () => {
-    const myPost = createNewPost();
-    myPost.insertAdjacentElement('beforebegin', newPost);
+    const newPosts = createNewPost();
+    card.insertAdjacentHTML('beforebegin', newPosts);
 })
